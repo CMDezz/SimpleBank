16a6436c450a6e674b6cda92c1953d426ab721f0
Viet <levietviet44266@gmail.com>
2024-02-28 14:08:47 +0700

    tiep tuc tx

---
+++ db/sqlc/store.go
@@ -0,0 +1,61 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		Queries: New(db),
+		db:      db,
+	}
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := New(tx)
+
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("err: %v, errTx: %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountId   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount int64    `json:"from_account_id"`
+	ToAccount   int64    `json:"to_account_id"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		return nil
+	})
+
+	return result, err
+
+}
