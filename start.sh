fbea4f114582b2cff51b06b73aeb74d18cae50b8
viet <levietviet44266@gmail.com>
2024-03-22 20:53:05 +0700

    we

--- start.sh
+++ start.sh
@@ -3,12 +3,8 @@
 set -e
 
 echo "run db migration 2"
-cat /app/app.env
 source /app/app.env
-ls -l app
-ls -l app/migration
-ls -l /app
-ls -l /app/migration
+ls -l
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 
 echo "start the app"
